596718489ae74102314e3e53e33646e7d59353ed
HolyRaveOS <kitlaruk1995@gmail.com>
2024-05-21 17:32:19 +0300

    create server

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.HOST_URL;
+const server = express();
+
+server.use(cors({ origin: '*' }));
+
+server.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
