407406a30713ee22798368a763d65436eba6a3e7
Lesia <voloshka2912@icloud.com>
2024-05-21 21:02:54 +0300

    add ESLint to GitHub

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 
-const PORT = process.env.PORT ?? 3000;
-const server = express();
+const PORT = process.env.PORT ?? 3000
+const server = express()
 
-server.use(cors({ origin: '*' }));
+server.use(cors({ origin: '*' }))
 
 server.get('*', (req, res) => {
-  res.send('Hello, World!');
-});
+  res.send('Hello, World!')
+})
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
