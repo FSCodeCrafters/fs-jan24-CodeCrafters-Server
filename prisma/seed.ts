62949bcbd210a7fbee23deb816e2c70f96bca3f2
HolyRaveOS <kitlaruk1995@gmail.com>
2024-05-21 18:17:18 +0300

    add vercel config

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,10 +4,9 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function main() {
+async function main(): Promise<void> {
   try {
     await prisma.product.deleteMany({});
-
     await prisma.productItem.deleteMany({});
 
     await prisma.productItem.createMany({
@@ -20,6 +19,8 @@ async function main() {
     await prisma.product.createMany({
       data: products.map((product) => product),
     });
+
+    return;
   } catch (e) {
     console.error(e);
     process.exit(1);
@@ -28,4 +29,7 @@ async function main() {
   }
 }
 
-main();
+void main().catch((error) => {
+  console.error('Error during execution:', error);
+  process.exit(1);
+});
