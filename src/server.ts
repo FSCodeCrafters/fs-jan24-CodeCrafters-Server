d030fddb9bc60122d8ce03d34aca5f4d4353825c
HolyRaveOS <kitlaruk1995@gmail.com>
2024-05-21 18:30:29 +0300

    made corrections

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,20 @@
-'use strict'
+'use strict';
 
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.HOST_URL
-const server = express()
+const PORT = process.env.PORT;
+const server = express();
 
-server.use(cors({ origin: '*' }))
+server.use(cors({ origin: '*' }));
 
 server.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
+  res.send('Hello, World!');
+});
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
-
-export default server
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
