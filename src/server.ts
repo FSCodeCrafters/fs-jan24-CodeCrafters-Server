33c00a0d6c9c6fc7c78529d162f3fb2c242efeea
Yevhen Vladarskyi <vladarskyy@gmail.com>
2024-05-23 13:46:30 +0300

    change eslint config

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ server.use(cors());
 server.use('/products', express.json(), productRouter);
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
