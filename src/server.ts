8610779ec33b3aba4a145dd41a043db340e34153
HolyRaveOS <kitlaruk1995@gmail.com>
2024-05-24 19:20:00 +0300

    add authentication with google

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,35 @@
 'use strict';
 
 import express from 'express';
+import session from 'express-session';
 import cors from 'cors';
 import { router as productRouter } from './routes/product.route';
 import { router as productItem } from './routes/productItem.route';
+import { router as authRouter } from './routes/auth.route';
+import passport from 'passport';
+import './strategies/google-strategy';
 
 const PORT = process.env.PORT ?? 3005;
 const server = express();
 
 server.use(cors());
+server.use(express.json());
+
+server.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'default_secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  }),
+);
+
+server.use(passport.initialize());
+server.use(passport.session());
 
 server.use('/products', express.json(), productRouter);
 server.use('/productItem', express.json(), productItem);
+server.use('/', express.json(), authRouter);
 
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
---
+++ src/strategies/google-strategy.ts
@@ -0,0 +1,63 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+import { db } from '../db';
+dotenv.config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL as string,
+      scope: ['profile', 'email'],
+    },
+    async (_accessToken, _refreshToken, profile, done) => {
+      try {
+        if (profile.emails && profile.emails.length > 0) {
+          const email = profile.emails[0].value;
+
+          const findUser = await db.user.findUnique({
+            where: { email },
+          });
+
+          if (findUser) {
+            return done(null, findUser);
+          } else {
+            const newUser = await db.user.create({
+              data: {
+                email,
+                name: profile.displayName || null,
+                image:
+                  profile.photos && profile.photos.length > 0
+                    ? profile.photos[0].value
+                    : null,
+              },
+            });
+            return done(null, newUser);
+          }
+        } else {
+          return done(new Error('No email found in user profile'), false);
+        }
+      } catch (err) {
+        return done(err, false);
+      }
+    },
+  ),
+);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+passport.serializeUser((user: any, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id: string, done) => {
+  try {
+    const user = await db.user.findUnique({
+      where: { id },
+    });
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
